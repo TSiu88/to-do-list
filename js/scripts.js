a3442f7c5a1b15c3e9fa0deb3fa76bad91495277
neha doodi and Tiffany Siu <neha@>
2020-01-27 16:43:50 -0800

    add delete from array when checked

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 // Business logic
-function Task() {
+function TaskList() {
   this.tasklist = [];
   this.currentId = 0;
 }
-Task.prototype.addTask = function(task) {
+TaskList.prototype.addTask = function(task) {
   task.id = this.assignId();
   this.tasklist.push(task);
   
 }
-Task.prototype.assignId = function() {
+TaskList.prototype.assignId = function() {
   this.currentId +=1;
   
   return this.currentId;
 }
-Task.prototype.deleteTask = function(id) {
+TaskList.prototype.deleteTask = function(id) {
   for (var i=0; i<this.tasklist.length; i++) {
     if(this.tasklist[i]) {
-      if(this.tasklist[i].id === id) {
+      if(this.tasklist[i].id == id) {
         delete this.tasklist[i];
         return true;
       }
@@ -25,30 +25,59 @@ Task.prototype.deleteTask = function(id) {
   return false;
 }
 
+function Task(task){
+  this.task = task;
+}
 
 
 $(document).ready(function(){
-  var wholeList = new Task();
-  $("#formOne").submit(function(event) {
+  var wholeList = new TaskList();
+  $(".btn").on("click", function(){
     event.preventDefault();
     var task = $("input#task").val();
-    if (task === ""){
-      alert("Invalid entry");
-    }
-    else {
-     
-      wholeList.addTask(task);
+    var buttonId = this.id;
+
+    if (task !== "" && buttonId === "submit"){
+      var currentTask = new Task(task);
+      wholeList.addTask(currentTask);
       console.log(wholeList.tasklist);
       var checkbox = document.createElement("input");
       checkbox.type = "checkbox";
-      checkbox.id = task;
+      checkbox.id = currentTask.id;
+      checkbox.value = currentTask.id;
+      console.log("checkbox id",checkbox.id);
       checkbox.name = task;
-      console.log(task.id);
+      var label = document.createElement("label");
+      label.innerHTML = task;
+      label.id = currentTask.id;
+      console.log(label.id);
       $("#checklist").append(checkbox);
-      $("#checklist").append(" " + task + "<br>");
+      $("#checklist").append(label);
+      $("#checklist").append(document.createElement("br"));
+      
+    }
+    else if (buttonId === "delete"){
+      var n = $("input:checked").length;
+      console.log("num checked", n);
+      if (n > 0){
+        $("input:checked").each(function(){
+          var removeId = $("input:checked").val();
+          console.log(removeId);
+          var removed = wholeList.deleteTask(removeId);
+          console.log(wholeList.tasklist);
+          //Does delete out of array, Need to delete checkbox and text
+        });
+        
+        
+      }else{
+        alert("No entries checked for deletion")
+      }
+    }
+    else {
+      alert("Invalid entry");
     }
 
-
+    
 
   });
 });
